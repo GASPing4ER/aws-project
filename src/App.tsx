d2867be2fd515dc049be1beab784f52a49b5d088
GASPing4ER <simecgasper@gmail.com>
2024-08-23 18:34:47 +0200

    changes for amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
 export default function App() {
   return (
     <div>
-      <button className="text-slate-900">Sign Out</button>
+      <Authenticator>
+        {({ signOut }) => (
+          <main>
+            <h1>Hello</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
